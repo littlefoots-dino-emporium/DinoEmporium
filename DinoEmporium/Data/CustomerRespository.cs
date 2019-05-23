0c5ed69984bf75415693a1ce1f7fbbb3661c2d67
unknown <f.mulugeta90@gmail.com>
2019-05-23 14:54:06 -0500

    Creating customers in progress

---
+++ DinoEmporium/Data/CustomerRespository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Dapper;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DinoEmporium.Models;
+using System.Data.SqlClient;
+
+namespace DinoEmporium.Data
+{
+    public class CustomerRespository
+    {
+        const string ConnectionString = "Server=localhost;Database=Littlefoots;Trusted_Connection=True;";
+
+        public Customer AddCustomer(string firstName, string lastName, DateTime dateTime, string email)
+        {
+            using (var db = new SqlConnection(ConnectionString))
+            {
+                var newCustomer = db.QueryFirstOrDefault<Customer>(@"
+                    Insert into users (firstName,lastName, dateTime, email)
+                    Output inserted.*
+                    Values(@firstName,@lastName,@dateTime,@email)",
+                    new { firstName, lastName, dateTime, email });
+
+                if (newCustomer != null)
+                {
+                    return newCustomer;
+                }
+            }
+
+            throw new Exception("No user created");
+        }
+
+        public Customer GetCustomers()
+        {
+            using (var db = new SqlConnection(ConnectionString))
+            {
+                var allCustomers = db.QueryFirstOrDefault<Customer>(@"Select * from Customer");
+                return allCustomers;
+            }
+        }
+    }
+}
---
+++ DinoEmporium/Models/CreateCustomerRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DinoEmporium.Models
+{
+    public class CreateCustomerRequest
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public DateTime Date { get; set; }
+        public string Email { get; set; }
+
+    }
+}
