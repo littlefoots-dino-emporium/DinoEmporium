79373bef3778c69b36ee1f2aace9f16b389011b5
Michelle Beshears <meeshysu@gmail.com>
2019-05-24 14:58:52 -0500

    saving progress thus far; properties created, working on controller

--- DinoEmporium/Models/Products.cs
+++ DinoEmporium/Models/Products.cs
@@ -7,5 +7,12 @@ namespace DinoEmporium.Models
 {
     public class Products
     {
+        public int Id { get; set; }
+        public int ProductTypeId { get; set; }
+        public decimal Price { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public decimal Quantity { get; set; }
+        public int CustomerId { get; set; }
     }
 }
