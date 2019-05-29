ce2be5464604d8071a70bdb967dcbae1f3b8ade3
Lance Hamilton <lance.hamilton24@gmail.com>
2019-05-29 16:05:52 -0500

    added order

--- DinoEmporium/Data/OrderRepository.cs
+++ DinoEmporium/Data/OrderRepository.cs
@@ -1,11 +1,33 @@
-﻿using System;
+﻿using Dapper;
+using DinoEmporium.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace DinoEmporium.Data
 {
-    public class UserRepository
+    public class OrderRepository
     {
+        const string ConnectionString = "Server=localhost;Database=Littlefoots;Trusted_Connection=True;";
+
+        public Order AddOrder(int price)
+        {
+            using (var db = new SqlConnection(ConnectionString))
+            {
+                var newOrder = db.QueryFirstOrDefault<Order>(@"
+                    Insert into [Order] (price)
+                    Output inserted.*
+                    Values(@price)",
+                    new { price });
+
+                if (newOrder != null)
+                {
+                    return newOrder;
+                }
+            }
+            throw new Exception("No order created");
+        }
     }
 }
