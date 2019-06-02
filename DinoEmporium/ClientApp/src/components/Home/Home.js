b015c7a208b2e016b7db28350b9aea6c29b53119
Michelle Beshears <meeshysu@gmail.com>
2019-06-02 17:08:09 -0500

    adjusting and adding

--- DinoEmporium/ClientApp/src/components/Home/Home.js
+++ DinoEmporium/ClientApp/src/components/Home/Home.js
@@ -1,27 +1,9 @@
 import React, { Component } from 'react';
-import fencingShape from '../../../helpers/propz/fencingShape';
-import fenceRequests from '../../../helpers/data/fenceRequest'
+//import fencingShape from '../../helpers/propz/fencingShape';
+//import fenceRequests from '../../helpers/data/fenceRequest';
 
 
- class FenceItem extends React.Component {
-   static propTypes = {
-     fence: fencingShape.fencingShape,
-   }
- 
-
-  render () {
-    const { fence } = this.props;
-    const fenceId = fenceRequests.getRequest();
-    console.log(fenceId);
-    return (
-      <div className="Home-Product card-group">
-        <span className='fence-image'>{fence.image}</span>
-        <span className='fence-structure'>{fence.structure}</span>
-        <span className='fence-price'>{fence.price}</span>
-        <span className='fence-description'>{fence.description}</span>
-      </div>
-    );
-  }
+ class Home extends React.Component {
  }
 
-export default FenceItem;
+export default Home;
---
+++ DinoEmporium/ClientApp/src/components/HomeItem/HomeItem.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './HomeItem.scss';
+
+const defaultValue = {
+  fenceId: '',
+  dinosaurId: '',
+  sweaterId: '',
+}
+
+class ProductItem extends React.Component {
+  static propTypes = {
+    
+  }
+}
