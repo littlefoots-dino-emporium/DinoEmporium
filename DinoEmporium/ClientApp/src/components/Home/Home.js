ebde98e51290cb60eb4f7972c99cd86e2b23709c
Michelle Beshears <meeshysu@gmail.com>
2019-06-02 16:45:07 -0500

    created fenceRequest and fenceShape

--- DinoEmporium/ClientApp/src/components/Home/Home.js
+++ DinoEmporium/ClientApp/src/components/Home/Home.js
@@ -1,20 +1,27 @@
 import React, { Component } from 'react';
 import fencingShape from '../../../helpers/propz/fencingShape';
-
+import fenceRequests from '../../../helpers/data/fenceRequest'
 
 
  class FenceItem extends React.Component {
    static propTypes = {
      fence: fencingShape.fencingShape,
    }
- }
+ 
 
   render () {
+    const { fence } = this.props;
+    const fenceId = fenceRequests.getRequest();
+    console.log(fenceId);
     return (
       <div className="Home-Product card-group">
+        <span className='fence-image'>{fence.image}</span>
+        <span className='fence-structure'>{fence.structure}</span>
+        <span className='fence-price'>{fence.price}</span>
+        <span className='fence-description'>{fence.description}</span>
       </div>
     );
   }
-
+ }
 
 export default FenceItem;
