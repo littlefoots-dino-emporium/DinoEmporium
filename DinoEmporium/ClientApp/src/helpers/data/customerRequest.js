8072eaba81bd926e01843f2ee4a17bda89d0abdf
Lance Hamilton <lance.hamilton24@gmail.com>
2019-06-05 12:45:23 -0500

    wip

--- DinoEmporium/ClientApp/src/helpers/data/customerRequest.js
+++ DinoEmporium/ClientApp/src/helpers/data/customerRequest.js
@@ -2,40 +2,26 @@ import axios from 'axios';
 import apiKeys from '../../firebaseRequests/apiKeys';
 
 
-const firebaseUrl = apiKeys.firebaseConfig.databaseURL;
-// const getCustomerProfile = () => new Promise((resolve, reject) => {
-//   axios
-//     .get(`https://localhost:50312/api/customer/allCustomers`)
-//     .then((res) => {
-//       console.log(res);
+// const firebaseUrl = apiKeys.firebaseConfig.databaseURL;
+const getCustomerProfile = () => new Promise((resolve, reject) => {
+  axios
+    .get(`http://localhost:50312/api/customer/1`)
+    .then((res) => {
+      console.log(res.data);
 
-//       // let customer = '';
-//       // if (res.data !== null) {
-//       //   Object.keys(res.data).forEach((key) => {
-//       //     res.data[key].id = key;
-//       //     customer = res.data[key];
-//       //   });
-//       // }
-//       // resolve(customer);
-//     })
-//     .catch(err => reject(err));
-// });
+      // let customer = '';
+      // if (res.data !== null) {
+      //   Object.keys(res.data).forEach((key) => {
+      //     res.data[key].id = key;
+      //     customer = res.data[key];
+      //   });
+      // }
+      // resolve(customer);
+    })
+    .catch(err => reject(err));
+});
 
-const getCustomerProfile = (id) => {
-  return new Promise((resolve, reject) => {
-    axios
-      .get(`http://localhost:50312/api/customer/1`)
-      .then(res => {
-        const customers = [];
-        resolve(customers);
-      })
-      .catch(err => {
-        reject(err);
-      });
-  });
-}
-
-const postCustomerRequest = (customerEmail, customerName, customerUid) => axios.post(`${firebaseUrl}/customer.json`, customerEmail,customerName,customerUid);
+const postCustomerRequest = (customerInfo) => axios.post(`http://localhost:50312/api/customer/register`, customerInfo);
 
 export default {
   postCustomerRequest,
--- DinoEmporium/Models/Customer/Customer.cs
+++ DinoEmporium/Models/Customer/Customer.cs
@@ -13,6 +13,8 @@ namespace DinoEmporium.Models
         public string LastName { get; set; }
         public DateTime Date { get; set; }
         public string Email { get; set; }
+        public string CustomerUid { get; set; }
+        public int ProductId { get; set; }
     }
    
 }
