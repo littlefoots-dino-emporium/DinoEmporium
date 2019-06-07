2abc52915b57a475228de5a9bfc16cd3f0b4ee2c
Michelle Beshears <meeshysu@gmail.com>
2019-06-07 14:43:24 -0500

    saving

--- DinoEmporium/ClientApp/src/components/Home/Home.js
+++ DinoEmporium/ClientApp/src/components/Home/Home.js
@@ -1,42 +1,62 @@
 import React from 'react';
 import productRequests from '../../helpers/data/productRequest';
 import ProductItem from '../ProductItem/ProductItem';
+import Modal from 'react-responsive-modal';
+import { Button } from 'react';
 
 
+class Home extends React.Component {
 
- class Home extends React.Component {
-     state = {
-         product: [], 
-     }
-     componentDidMount() {
-        this.allProducts();
-        }
+  state = {
+    product: [],
+    open: false,
+  }
 
-         allProducts = () => {
-             productRequests.getRequest().then((product) => {
-                 this.setState({ product });
-                 console.log(product);
-             })
-         }
+  componentDidMount() {
+    this.allProducts();
+  }
 
-         render() {
-             const { product } = this.state;
-             const productItemComponent = product.map(product => (
-                 <ProductItem
-                 product = {product}
-                 key = {product.id}
-                 />
-             ));
+  allProducts = () => {
+    productRequests.getRequest().then((product) => {
+      this.setState({ product });
+      console.log(product);
+    })
+  }
 
-             return (
-                 <div>
-                     <div className="productCards">
-                         {productItemComponent}
-                     </div>
-                 </div>
-             )
-         }
-     }
+  onOpenModal = () => {
+    this.setState({ open: true });
+  };
+
+  onCloseModal = () => {
+    this.setState({ open: false });
+  };
+
+  render() {
+    const { product, open } = this.state;
+    const productItemComponent = product.map(product => (
+      <ProductItem
+        product={product}
+        key={product.id}
+        onOpenModal={this.onOpenModal}
+      />
+    ));
+
+    return (
+      <div className='productStuff'>{productItemComponent}</div>
+
+
+      
+        <Modal className="modal" open={open} onClose={this.onCloseModal} center>
+        <img className='product-image' src={product.image} alt='product' />
+        <li className='product-title'>{product.title}</li>
+        <li className='plant-price'><i>${product.price}</i></li>
+        <li className='plant-description'>{product.description}</li>
+        <li className='plant-quantity'>We have <b>{product.quantity}</b> in stock.</li>
+        </Modal>
+        <ModalFooter>
+    )
+  }
+}
 
 
 export default Home;
--- DinoEmporium/ClientApp/src/components/ProductItem/ProductItem.js
+++ DinoEmporium/ClientApp/src/components/ProductItem/ProductItem.js
@@ -1,22 +1,36 @@
 import React from 'react';
+//import Modal from 'react-responsive-modal';
+import { Button } from 'react';
 import './ProductItem.scss';
 
 class ProductItem extends React.Component {
 
 
     render() {
-        const { product } = this.props;
-        return (
+        //const { products } = this.props;
+        const infoButton = () => (
             <div>
-                <div className="card-group">
-                    <ul className="card bg-transparent">
-                        <img className='product-image' src={product.image} alt='product' />
-                        <ul className='card-body'>
-                            <li className='product-title'>{product.title}</li>
-                            <li className='plant-price'><i>{product.price}</i></li>
-                            <li className='plant-description'>{product.description}</li>
-                        </ul>
-                    </ul>
+                <span className="openForInfo col">
+                    <Button outline color="info" onClick={this.openForInfo}>
+                        Information
+                </Button>
+                </span>
+            </div>
+        );
+        const cartButton = () => (
+            <div>
+                <span className="addToCart col">
+                    <Button outline color="danger" onClick={this.addToCart}>
+                        Add To Cart
+                </Button>
+                </span>
+            </div>
+        );
+        return (
+            <div className='productCard card'>
+                <div className='text-center card-header'>
+                    <span className='col-3' onClick={this.addToCart}>{cartButton()}</span>
+                    <span className='col-3' onClick={this.openForInfo}>{infoButton()}</span>
                 </div>
             </div>
         )
