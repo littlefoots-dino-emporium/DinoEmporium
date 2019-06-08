7f2d4a1a89e8bd7474d6dace826112d2712752dd
Michelle Beshears <meeshysu@gmail.com>
2019-06-07 19:31:24 -0500

    all products are individual and buttons show; product appears when clicked

--- DinoEmporium/ClientApp/src/components/Home/Home.js
+++ DinoEmporium/ClientApp/src/components/Home/Home.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import productRequests from '../../helpers/data/productRequest';
 import ProductItem from '../ProductItem/ProductItem';
-import Modal from '../../helpers/modal/Modal';
+
 
 
 class Home extends React.Component {
 
   state = {
     product: [],
-    showModal: false
   }
 
   componentDidMount() {
@@ -21,12 +20,6 @@ class Home extends React.Component {
     })
   }
 
-  toggleModal = () => {
-    this.setState({
-      showModal: !this.state.showModal
-    });
-  }
-
   render() {
     const { product } = this.state;
     const productItemComponent = product.map(product => (
@@ -37,22 +30,7 @@ class Home extends React.Component {
     ));
 
       return (
-      <div className="app">
-        <button className="modal_opener" onClick={this.toggleModal}>
-          Click Me! I Do Not Bite... <span role="img" aria-label="emoji">😛</span>
-        </button>
-
-        <Modal
-          show={this.state.showModal}
-          closeCallback={this.toggleModal}
-          customClass="custom_modal_class"
-        >
-          <React.Fragment>
-            <h2>Told Ya!</h2>
-            <p>{productItemComponent}</p>
-          </React.Fragment>
-        </Modal>
-      </div>
+        <p>{productItemComponent}</p>
     );
   }
 }
--- DinoEmporium/ClientApp/src/components/ProductItem/ProductItem.js
+++ DinoEmporium/ClientApp/src/components/ProductItem/ProductItem.js
@@ -1,21 +1,46 @@
 import React from 'react';
+import Modal from '../../helpers/modal/Modal';
 import './ProductItem.scss';
 
 class ProductItem extends React.Component {
 
+    state = {
+        showModal: false
+    }
+
+    toggleModal = () => {
+        this.setState({
+            showModal: !this.state.showModal
+        });
+    }
 
     render() {
         const { product } = this.props;
         return (
+            <div className="app">
+                <button className="modal_opener" onClick={this.toggleModal}>
+                    Product
+            </button>
 
-            <div className='card-body productCard'>
-            <img className='product-image' src={product.image} alt='product' />
-            <li className='product-title'>{product.title}</li>
-            <li className='plant-price'><i>${product.price}</i></li>
-            <li className='plant-description'>{product.description}</li>
-            <li className='plant-quantity'>We have <b>{product.quantity}</b> in stock.</li>
+                <Modal
+                    show={this.state.showModal}
+                    closeCallback={this.toggleModal}
+                    customClass="custom_modal_class"
+                >
+                    <React.Fragment>
+                        <div className='card-body productCard'>
+                            <img className='product-image' src={product.image} alt='product' />
+                            <li className='product-title'>{product.title}</li>
+                            <li className='plant-price'><i>${product.price}</i></li>
+                            <li className='plant-description'>{product.description}</li>
+                            <li className='plant-quantity'>We have <b>{product.quantity}</b> in stock.</li>
+                        </div>
+                    </React.Fragment>
+                </Modal>
             </div>
-        )}
+
+        )
+    }
 }
 
 export default ProductItem;
