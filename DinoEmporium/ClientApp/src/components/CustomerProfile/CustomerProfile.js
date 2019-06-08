29006dca50ffb5c8473a74f16556e3bf257427bc
Lance Hamilton <lance.hamilton24@gmail.com>
2019-06-08 12:32:06 -0500

    added edit form

--- DinoEmporium/ClientApp/src/components/CustomerProfile/CustomerProfile.js
+++ DinoEmporium/ClientApp/src/components/CustomerProfile/CustomerProfile.js
@@ -3,6 +3,7 @@ import { Button } from 'reactstrap';
 //import getCustomerInfo from '../../helpers/data/customerRequest';
 import getCustomerInfo from '../../helpers/data/customerRequest';
 import autheRequests from '../../firebaseRequests/auth';
+import EditCustomerForm from '../EditCustomerForm/EditCustomerForm';
 import './CustomerProfile.scss';
 
 
@@ -14,22 +15,22 @@ export class CustomerProfile extends React.Component {
   }
 
   componentDidMount() {
-  let uid = autheRequests.getUid();
-  getCustomerInfo.getCustomerProfile(uid).then((customer) => { 
-    this.setState({ customer })
-  });
-}
+    let uid = autheRequests.getUid();
+    getCustomerInfo.getCustomerProfile(uid).then((customer) => {
+      this.setState({ customer })
+    });
+  }
 
   render() {
     const { customer } = this.state;
     const makeButtons = () => (
-          <div>
-            <span className="editLineup col">
-              <Button outline color="info" onClick={this.editCustomer}>
-                Edit
+      <div>
+        <span className="editLineup col">
+          <Button outline color="info" onClick={this.editCustomer}>
+            Edit
               </Button>
-            </span>
-          </div>
+        </span>
+      </div>
     );
 
     return (
@@ -38,6 +39,13 @@ export class CustomerProfile extends React.Component {
         <h1>{customer.lastName}</h1>
         <h3>{customer.email}</h3>
         {makeButtons()}
+        <div className='lineupForm'>
+        <EditCustomerForm
+          onSubmit={this.formSubmitLineup}
+          // isEditing={isEditing}
+          // editId={editId}
+        />
+      </div>
       </div>
     )
   }
---
+++ DinoEmporium/ClientApp/src/components/EditCustomerForm/EditCustomerForm.js
@@ -0,0 +1,193 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import authRequests from '../../firebaseRequests/auth';
+import customerRequest from '../../helpers/data/customerRequest';
+import firebase from 'firebase';
+
+
+// import './Register.scss';
+
+const customerInformation = {
+  email: '',
+  password: '',
+  firstName: '',
+  lastName: '',
+  date: '',
+}
+class EditCustomerForm extends React.Component {
+  static propTypes = {
+    onSubmit: PropTypes.func,
+    isEditing: PropTypes.bool,
+    editId: PropTypes.string,
+  }
+
+  props = {
+      newCustomerInformation: customerInformation,    
+  };
+
+  signUp = ( newCustomerInformation) => {
+    firebase.auth().createUserWithEmailAndPassword(newCustomerInformation.email, newCustomerInformation.password).then((res) => {
+      newCustomerInformation.uid = authRequests.getUid();
+      const customerInformation = { firstName: newCustomerInformation.firstName,
+                        lastName: newCustomerInformation.lastName,
+                        email: newCustomerInformation.email,
+                        customerUid: newCustomerInformation.uid
+                        }
+      customerRequest.postCustomerRequest(customerInformation);
+      this.props.history.push('/');
+    }).catch(err => console.error('there was an error with auth', err));
+  }
+
+  formFieldStringState = (name,e) => {
+    e.preventDefault();
+    const tempInfo = { ...this.state.newCustomerInformation};
+    tempInfo[name] = e.target.value;
+    this.setState({ newCustomerInformation: tempInfo});
+  }
+  
+  // emailChange = e => {
+  //   this.formFieldStringState('name', e);
+  // };
+
+  firstNameChange = e => {
+    this.formFieldStringState('firstName', e);
+  };
+  
+  lastNameChange = e => {
+    this.formFieldStringState('lastName', e);
+  };
+
+  dateChange = e => {
+    this.formFieldStringState('date', e);
+  };
+
+  emailChange = e => {
+    this.formFieldStringState('email', e);
+  }
+
+  passwordChange = e => {
+    this.formFieldStringState('password', e);
+  }
+
+  // registerClickEvent = e => {
+  //   const { newCustomerInformation } = this.state;
+  //   e.preventDefault();
+  //   console.log('click');
+  //   authRequests
+  //     .registerUser(newCustomerInformation)
+  //     .then(() => {
+  //       this.props.history.push('/');
+  //     })
+  //     .catch(error => {
+  //       console.error('there was an error in registering', error);
+  //     });
+  // };
+
+
+  formSubmit = (e) => {
+    e.preventDefault();
+    const { onSubmit } = this.props;
+    const userInformation = { ...this.props.newCustomerInformation };
+    userInformation.uid = authRequests.Uid();
+    onSubmit(userInformation);
+    this.setState({ newCustomerInformation: customerInformation });
+  }
+
+  componentDidUpdate(prevProps) {
+    const { isEditing, editId } = this.props;
+    if (prevProps !== this.props && isEditing) {
+      customerRequest.getSingleCustomer(editId)
+        .then((customer) => {
+          this.setState({ newCustomer : customer.data });
+        })
+        .catch(err => console.error('error with getSingleListing', err));
+    }
+  }
+
+  render () {
+    const { newCustomerInformation } = this.props;
+    return (
+      <div className="Register">
+        <div id="login-form">
+          <h1 className="text-center">Register</h1>
+          <form className="form-horizontal col-sm-6 col-sm-offset-3">
+            <div className="form-group">
+              <label htmlFor="inputName" className="col-sm-4 control-label">
+                First Name:
+              </label>
+              <div className="col-sm-8">
+                <input
+                  type="name"
+                  className="form-control"
+                  id="inputEmail"
+                  placeholder="First Name"
+                  // value={newCustomerInformation.firstName}
+                  // onChange={this.firstNameChange}
+                />
+              </div>
+            </div>
+            <div className="form-group">
+              <label htmlFor="inputLastName" className="col-sm-4 control-label">
+                Last Name:
+              </label>
+              <div className="col-sm-8">
+                <input
+                  type="name"
+                  className="form-control"
+                  id="inputEmail"
+                  placeholder="Last Name"
+                  // value={newCustomerInformation.lastName}
+                  // onChange={this.lastNameChange}
+                />
+              </div>
+            </div>
+            <div className="form-group">
+              <label htmlFor="inputEmail" className="col-sm-4 control-label">
+                Email:
+              </label>
+              <div className="col-sm-8">
+                <input
+                  type="email"
+                  className="form-control"
+                  id="inputEmail"
+                  placeholder="Email"
+                  // value={newCustomerInformation.email}
+                  // onChange={this.emailChange}
+                />
+              </div>
+            </div>
+            <div className="form-group">
+              <label htmlFor="inputPassword" className="col-sm-4 control-label">
+                Password:
+              </label>
+              <div className="col-sm-8">
+                <input
+                  type="password"
+                  className="form-control"
+                  id="inputPassword"
+                  placeholder="Password"
+                  // value={newCustomerInformation.password}
+                  // onChange={this.passwordChange}
+                />
+              </div>
+            </div>
+            <div className="form-group">
+              <div className="col-sm-12">
+                {/* <button
+                  type="submit"
+                  className="btn btn-default col-xs-12"
+                  onClick={this.formSubmit}
+                >
+                  Register
+                </button> */}
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default EditCustomerForm;
