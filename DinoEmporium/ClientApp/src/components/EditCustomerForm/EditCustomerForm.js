7e0fc676c873d35ce0e966f8520fe9883f4ed600
Lance Hamilton <lance.hamilton24@gmail.com>
2019-06-09 06:58:06 -0500

    edit customer form recognizes isEditing state

--- DinoEmporium/ClientApp/src/components/EditCustomerForm/EditCustomerForm.js
+++ DinoEmporium/ClientApp/src/components/EditCustomerForm/EditCustomerForm.js
@@ -26,18 +26,18 @@ class EditCustomerForm extends React.Component {
       newCustomerInformation: customerInformation,    
   };
 
-  signUp = ( newCustomerInformation) => {
-    firebase.auth().createUserWithEmailAndPassword(newCustomerInformation.email, newCustomerInformation.password).then((res) => {
-      newCustomerInformation.uid = authRequests.getUid();
-      const customerInformation = { firstName: newCustomerInformation.firstName,
-                        lastName: newCustomerInformation.lastName,
-                        email: newCustomerInformation.email,
-                        customerUid: newCustomerInformation.uid
-                        }
-      customerRequest.postCustomerRequest(customerInformation);
-      this.props.history.push('/');
-    }).catch(err => console.error('there was an error with auth', err));
-  }
+  // signUp = ( newCustomerInformation) => {
+  //   firebase.auth().createUserWithEmailAndPassword(newCustomerInformation.email, newCustomerInformation.password).then((res) => {
+  //     newCustomerInformation.uid = authRequests.getUid();
+  //     const customerInformation = { firstName: newCustomerInformation.firstName,
+  //                       lastName: newCustomerInformation.lastName,
+  //                       email: newCustomerInformation.email,
+  //                       customerUid: newCustomerInformation.uid
+  //                       }
+  //     customerRequest.postCustomerRequest(customerInformation);
+  //     this.props.history.push('/');
+  //   }).catch(err => console.error('there was an error with auth', err));
+  // }
 
   formFieldStringState = (name,e) => {
     e.preventDefault();
@@ -106,85 +106,173 @@ class EditCustomerForm extends React.Component {
   }
 
   render () {
-    const { newCustomerInformation } = this.props;
-    return (
-      <div className="Register">
-        <div id="login-form">
-          <h1 className="text-center">Register</h1>
-          <form className="form-horizontal col-sm-6 col-sm-offset-3">
-            <div className="form-group">
-              <label htmlFor="inputName" className="col-sm-4 control-label">
-                First Name:
-              </label>
-              <div className="col-sm-8">
-                <input
-                  type="name"
-                  className="form-control"
-                  id="inputEmail"
-                  placeholder="First Name"
-                  // value={newCustomerInformation.firstName}
-                  // onChange={this.firstNameChange}
-                />
+    const { newCustomerInformation, isEditing } = this.props;
+
+      const title = () => {
+        if(isEditing) {
+          return (
+            <div className="Register">
+              <div id="login-form">
+                <h1 className="text-center">Update Customer</h1>
+                <form className="form-horizontal col-sm-6 col-sm-offset-3">
+                  <div className="form-group">
+                    <label htmlFor="inputName" className="col-sm-4 control-label">
+                      First Name:
+                    </label>
+                    <div className="col-sm-8">
+                      <input
+                        type="name"
+                        className="form-control"
+                        id="inputEmail"
+                        placeholder="First Name"
+                        // value={newCustomerInformation.firstName}
+                        // onChange={this.firstNameChange}
+                      />
+                    </div>
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="inputLastName" className="col-sm-4 control-label">
+                      Last Name:
+                    </label>
+                    <div className="col-sm-8">
+                      <input
+                        type="name"
+                        className="form-control"
+                        id="inputEmail"
+                        placeholder="Last Name"
+                        // value={newCustomerInformation.lastName}
+                        // onChange={this.lastNameChange}
+                      />
+                    </div>
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="inputEmail" className="col-sm-4 control-label">
+                      Email:
+                    </label>
+                    <div className="col-sm-8">
+                      <input
+                        type="email"
+                        className="form-control"
+                        id="inputEmail"
+                        placeholder="Email"
+                        // value={newCustomerInformation.email}
+                        // onChange={this.emailChange}
+                      />
+                    </div>
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="inputPassword" className="col-sm-4 control-label">
+                      Password:
+                    </label>
+                    <div className="col-sm-8">
+                      <input
+                        type="password"
+                        className="form-control"
+                        id="inputPassword"
+                        placeholder="Password"
+                        // value={newCustomerInformation.password}
+                        // onChange={this.passwordChange}
+                      />
+                    </div>
+                  </div>
+                  <div className="form-group">
+                    <div className="col-sm-12">
+                      {/* <button
+                        type="submit"
+                        className="btn btn-default col-xs-12"
+                        onClick={this.formSubmit}
+                      >
+                        Register
+                      </button> */}
+                    </div>
+                  </div>
+                </form>
               </div>
             </div>
-            <div className="form-group">
-              <label htmlFor="inputLastName" className="col-sm-4 control-label">
-                Last Name:
-              </label>
-              <div className="col-sm-8">
-                <input
-                  type="name"
-                  className="form-control"
-                  id="inputEmail"
-                  placeholder="Last Name"
-                  // value={newCustomerInformation.lastName}
-                  // onChange={this.lastNameChange}
-                />
+          )} return (
+            <div className="Register">
+              <div id="login-form">
+                <h1 className="text-center">Cust Info</h1>
+                <form className="form-horizontal col-sm-6 col-sm-offset-3">
+                  <div className="form-group">
+                    <label htmlFor="inputName" className="col-sm-4 control-label">
+                      First Name:
+                    </label>
+                    <div className="col-sm-8">
+                      <input
+                        type="name"
+                        className="form-control"
+                        id="inputEmail"
+                        placeholder="First Name"
+                        // value={newCustomerInformation.firstName}
+                        // onChange={this.firstNameChange}
+                      />
+                    </div>
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="inputLastName" className="col-sm-4 control-label">
+                      Last Name:
+                    </label>
+                    <div className="col-sm-8">
+                      <input
+                        type="name"
+                        className="form-control"
+                        id="inputEmail"
+                        placeholder="Last Name"
+                        // value={newCustomerInformation.lastName}
+                        // onChange={this.lastNameChange}
+                      />
+                    </div>
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="inputEmail" className="col-sm-4 control-label">
+                      Email:
+                    </label>
+                    <div className="col-sm-8">
+                      <input
+                        type="email"
+                        className="form-control"
+                        id="inputEmail"
+                        placeholder="Email"
+                        // value={newCustomerInformation.email}
+                        // onChange={this.emailChange}
+                      />
+                    </div>
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="inputPassword" className="col-sm-4 control-label">
+                      Password:
+                    </label>
+                    <div className="col-sm-8">
+                      <input
+                        type="password"
+                        className="form-control"
+                        id="inputPassword"
+                        placeholder="Password"
+                        // value={newCustomerInformation.password}
+                        // onChange={this.passwordChange}
+                      />
+                    </div>
+                  </div>
+                  <div className="form-group">
+                    <div className="col-sm-12">
+                      {/* <button
+                        type="submit"
+                        className="btn btn-default col-xs-12"
+                        onClick={this.formSubmit}
+                      >
+                        Register
+                      </button> */}
+                    </div>
+                  </div>
+                </form>
               </div>
             </div>
-            <div className="form-group">
-              <label htmlFor="inputEmail" className="col-sm-4 control-label">
-                Email:
-              </label>
-              <div className="col-sm-8">
-                <input
-                  type="email"
-                  className="form-control"
-                  id="inputEmail"
-                  placeholder="Email"
-                  // value={newCustomerInformation.email}
-                  // onChange={this.emailChange}
-                />
-              </div>
-            </div>
-            <div className="form-group">
-              <label htmlFor="inputPassword" className="col-sm-4 control-label">
-                Password:
-              </label>
-              <div className="col-sm-8">
-                <input
-                  type="password"
-                  className="form-control"
-                  id="inputPassword"
-                  placeholder="Password"
-                  // value={newCustomerInformation.password}
-                  // onChange={this.passwordChange}
-                />
-              </div>
-            </div>
-            <div className="form-group">
-              <div className="col-sm-12">
-                {/* <button
-                  type="submit"
-                  className="btn btn-default col-xs-12"
-                  onClick={this.formSubmit}
-                >
-                  Register
-                </button> */}
-              </div>
-            </div>
-          </form>
-        </div>
+          )
+        };
+      return (
+        <div className="editCustomer">
+        {title()}
       </div>
     );
   }
