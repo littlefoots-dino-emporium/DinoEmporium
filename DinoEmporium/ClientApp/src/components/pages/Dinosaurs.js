68d05ca544b07c4feb6ed0323242857e9abbe041
Michelle Beshears <meeshysu@gmail.com>
2019-06-13 19:59:01 -0500

    working on search field

--- DinoEmporium/ClientApp/src/components/pages/Dinosaurs.js
+++ DinoEmporium/ClientApp/src/components/pages/Dinosaurs.js
@@ -1,35 +1,63 @@
 import React from 'react';
 import DinosaurItem from '../DinosaurItem/DinosaurItem';
 import productTypeRequests from '../../helpers/data/productTypeRequest';
+import SearchField from 'react-search-field';
 
 
 class Dinosaur extends React.Component {
   state = {
-    productType: [],
+    product: [],
+    filteredDinosaurs: [],
   }
 
-  componentDidMount() {
-    this.allDinos();
-  }
-
-  allDinos = () => {
-    productTypeRequests.getDinoRequest().then((productType) => {
-      this.setState({ productType });
+  allDinosComponentDidMount = () => {
+    productTypeRequests.getDinoRequest().then((product) => {
+      this.setState({ product });
+      this.setState({ filteredDinosaurs: product });
     })
   }
 
+  onChange = (value, e) => {
+    const { product } = this.state;
+    const filteredDinosaurs = [];
+    e.preventDefault();
+    if (!value) {
+      this.setState({ filteredDinosaurs: product });
+    } else {
+      product.forEach((product) => {
+        if (product.title.toLowerCase().includes(value.toLowerCase())
+          || product.price.toLowerCase().includes(value.toLowerCase())
+          || product.size.toLowerCase().includes(value.toLowerCase())
+        ) {
+          filteredDinosaurs.push(product);
+        }
+        this.setState({ filteredDinosaurs });
+      });
+    }
+  }
+
   render() {
-    const { productType } = this.state;
-    const dinosaurItemComponent = productType.map(productType => (
-      <DinosaurItem
-        product={productType}
-        key={productType.id}
-      />
+    const { product } = this.state;
+    const dinosaurItemComponent = product.map(product => (
+      <div>
+        <DinosaurItem 
+          product={product}
+          key={product.id}
+        />
+        </div>
     ));
 
     return (
+      <div>
+      <SearchField
+      placeholder="Search Dinosaurs..."
+      onChange={this.onChange}
+      searchText=""
+      classNames="test-class w-50 mt-auto"
+    />
       <div className='dinosaurs'>
         {dinosaurItemComponent}
+      </div>
       </div>
     );
   }
