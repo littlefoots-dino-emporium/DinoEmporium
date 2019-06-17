ca7b309324b4a7b211a7aa56fcc1846cd511d843
unknown <f.mulugeta90@gmail.com>
2019-06-17 18:15:11 -0500

    get payment information

--- DinoEmporium/ClientApp/src/components/PaymentInformation/PaymentInformation.js
+++ DinoEmporium/ClientApp/src/components/PaymentInformation/PaymentInformation.js
@@ -1,16 +1,45 @@
-
+import React from 'react';
+import Modal from 'react-responsive-modal';
+import { Button } from 'reactstrap';
+import paymentRequest from '../../helpers/data/paymentInformation';
+import authRequest from '../../firebaseRequests/auth';
 
 
 class PaymentInformation extends React.Component {
 
     state = {
-        
+        paymentInfo: [],
+        open: false
     }
+    componentDidMount() {
+        const uid = authRequest.getUid();
+        
+    paymentRequest.getPaymentInformation(uid).then((paymentInfo) => {
+        this.setState({ paymentInfo })
+        console.log(paymentInfo);
+    })
+}
+onOpenModal = () => {
+    this.setState({ open: true });
+  };
+ 
+  onCloseModal = () => {
+    this.setState({ open: false });
+  };
 
 
 render(){
-
+    const { open } = this.state;
+  const { paymentInfo } = this.state;
+  return (
+  <div>
+      <Button onClick={this.onOpenModal}>Add Payment Information</Button>
+      <Modal open={open} onClose={this.onCloseModal} center>
+          
+        </Modal>
+      {paymentInfo}
+  </div>
+  )};
 }
 
-
-}
+export default PaymentInformation;
