54069a396617d146e0bc17da43cabfba1bebf1a6
Lance Hamilton <lance.hamilton24@gmail.com>
2019-06-19 19:33:31 -0500

    added carousel to home page

--- DinoEmporium/ClientApp/src/components/Home/Home.js
+++ DinoEmporium/ClientApp/src/components/Home/Home.js
@@ -30,15 +30,38 @@ class Home extends React.Component {
       />
 
     return (
-        <div className='productLinks row'>
+      <div>
+      <div className='productLinks row'>
 
-          <Link to="/dinosaurs" className="homeLink"><Button className="btn dinosaurLinkToPge" color="info"> Dinosaurs</Button></Link>
+        <Link to="/dinosaurs" className="homeLink"><Button className="btn dinosaurLinkToPge" color="info"> Dinosaurs</Button></Link>
 
-          <Link to="/sweaters" className="homeLink"><Button className="btn sweaterLinkToPge" color="info">Sweaters</Button></Link>
+        <Link to="/sweaters" className="homeLink"><Button className="btn sweaterLinkToPge" color="info">Sweaters</Button></Link>
 
-          <Link to="/fences" className="homeLink"><Button className="btn fenceLinkToPge" color="info">Fences</Button></Link>
+        <Link to="/fences" className="homeLink"><Button className="btn fenceLinkToPge" color="info">Fences</Button></Link>
 
+      </div>
+      <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img class="d-block w-100" src="http://i66.tinypic.com/5kp120.jpg" alt="First slide"></img>
+            </div>
+            <div class="carousel-item active">
+              <img class="d-block w-100" src="http://i63.tinypic.com/23lf6ok.jpg" alt="Second slide"></img>
+            </div>
+            <div class="carousel-item">
+              <img class="d-block w-100" src="http://i68.tinypic.com/2ikz91v.jpg" alt="Third slide"></img>
+            </div>
+          </div>
+          <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
         </div>
+      </div>
     );
   }
 }
