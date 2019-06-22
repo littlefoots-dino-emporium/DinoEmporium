c1bde623eecf8004927bd773ac0ae21bbe4c5eef
unknown <f.mulugeta90@gmail.com>
2019-06-22 07:47:55 -0500

    some styling

--- DinoEmporium/ClientApp/src/components/PaymentItem/PaymentItem.js
+++ DinoEmporium/ClientApp/src/components/PaymentItem/PaymentItem.js
@@ -15,6 +15,9 @@ class PaymentItem extends React.Component {
                 <h4>Account Number: {paymentInfo.accountNumber}</h4>
                 <h4>Expiration Date: {paymentInfo.expirationDate}</h4>
                 {/* <h3>PaymentType:{paymentInfo.paymentType}</h3>  */}
+                <button className="btn btn-danger delete-button" ><i class="fas fa-trash-restore"></i></button>
+                {/* <button className="btn btn-primary edit-button"><i class="fas fa-pencil-alt"></i></button> */}
+
             </div>
         )};
 
