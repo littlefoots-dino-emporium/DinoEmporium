3b627f8ffd77bce6b54a0ef7cac3413981f23628
Lance Hamilton <lance.hamilton24@gmail.com>
2019-06-24 21:00:14 -0500

    wip

--- DinoEmporium/ClientApp/src/components/OrderHistory/OrderHistory.js
+++ DinoEmporium/ClientApp/src/components/OrderHistory/OrderHistory.js
@@ -1,10 +1,31 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import authRequests from '../../firebaseRequests/auth';
+import getCustomerInfo from '../../helpers/data/customerRequest';
+import PropTypes from 'prop-types';
 
 export class OrderHistory extends Component {
+  state = {
+     customer: []
+  }
+
+  getCustomer = () => {
+    let uid = authRequests.getUid();
+    getCustomerInfo.getCustomerProfile(uid).then((customer) => {
+      this.setState({ customer })
+    });
+  }
+
+  componentDidMount() {
+    this.getCustomer();
+  }
+  
+
   render() {
+    const { customer } = this.state
     return (
       <div>
         <h1>Order History</h1>
+        <h2>{customer.firstName}</h2>
       </div>
     )
   }
