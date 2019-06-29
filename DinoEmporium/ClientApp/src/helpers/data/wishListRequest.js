fabf1badf33a2d7ecfa6cd162dc149f24ce6debf
Lance Hamilton <lance.hamilton24@gmail.com>
2019-06-29 12:35:08 -0500

    adding dinosaurs to wish list

--- DinoEmporium/ClientApp/src/helpers/data/wishListRequest.js
+++ DinoEmporium/ClientApp/src/helpers/data/wishListRequest.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+const postWishListRequest = (customerProductInfo) => axios.post(`http://localhost:50312/api/customerWishList/register`, customerProductInfo);
+
 const getWishListRequest = uid => new Promise((resolve, reject) => {
   axios
     .get(`http://localhost:50312/api/customerWishList/getCustomerWishList/${uid}`)
@@ -13,4 +15,5 @@ const getWishListRequest = uid => new Promise((resolve, reject) => {
 
 export default {
   getWishListRequest,
+  postWishListRequest,
 }
