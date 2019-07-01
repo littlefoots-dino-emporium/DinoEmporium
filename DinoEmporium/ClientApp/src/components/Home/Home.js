6e44ba703f8ba4e3b76187b812801f7b1e8d48a0
Michelle Beshears <meeshysu@gmail.com>
2019-06-30 19:23:29 -0500

    modal works now, put in new banner for deals, put in new banner for navbar

--- DinoEmporium/ClientApp/src/components/Home/Home.js
+++ DinoEmporium/ClientApp/src/components/Home/Home.js
@@ -3,13 +3,18 @@ import productRequests from '../../helpers/data/productRequest';
 import ProductItem from '../ProductItem/ProductItem';
 import { Link } from 'react-router-dom';
 import Carousel from 'react-bootstrap/Carousel';
+import Ducky from '../../Images/Ducky.png';
 import './Home.scss';
 
 class Home extends React.Component {
 
   state = {
     products: [],
+    index: 0,
+    direction: null
   }
+
+
 
   componentDidMount() {
     this.allProducts();
@@ -19,6 +24,13 @@ class Home extends React.Component {
     productRequests.getRequest().then((products) => {
       this.setState({ products });
     })
+  }
+
+  handleSelect(selectedIndex, e) {
+    this.setState({
+      index: selectedIndex,
+      direction: e.direction,
+    });
   }
 
   render() {
@@ -41,43 +53,89 @@ class Home extends React.Component {
             <Link to="/fences" className="fencesLink"><img className="lt3-image" src="http://i65.tinypic.com/a5f89j.jpg" alt="longneck"></img>Shop Fences</Link>
           </div>
 
-          <Carousel>
-            <Carousel.Item>
-              <img
-                className="d-block w-100"
-                src="holder.js/800x400?text=First slide&bg=373940"
-                alt="First slide"
-              />
-              <Carousel.Caption>
-                <h3>First slide label</h3>
-                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-              </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-              <img
-                className="d-block w-100"
-                src="holder.js/800x400?text=Second slide&bg=282c34"
-                alt="Third slide"
-              />
+      <img className="deal-image" src={Ducky} alt="ducky"></img>
+        <div className="whole-container-caurosel">
+          <div className="caurosel-container1">
+            <Carousel>
+              <Carousel.Item>
+                <img
+                  className="d-block"
+                  src="http://i68.tinypic.com/xgktgj.jpg"
+                  alt="First slide"
+                />
+                <Carousel.Caption>
+                  <h3>First slide label</h3>
+                  <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block"
+                  src="http://i66.tinypic.com/2z6uwpx.jpg"
+                  alt="Third slide"
+                />
 
-              <Carousel.Caption>
-                <h3>Second slide label</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-              </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-              <img
-                className="d-block w-100"
-                src="holder.js/800x400?text=Third slide&bg=20232a"
-                alt="Third slide"
-              />
+                <Carousel.Caption>
+                  <h3>Second slide label</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block"
+                  src="http://i64.tinypic.com/73ecr4.jpg"
+                  alt="Third slide"
+                />
 
-              <Carousel.Caption>
-                <h3>Third slide label</h3>
-                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-              </Carousel.Caption>
-            </Carousel.Item>
-          </Carousel>
+                <Carousel.Caption>
+                  <h3>Third slide label</h3>
+                  <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+            </Carousel>
+          </div>
+
+
+          <div className="caurosel-container2">
+            <Carousel>
+              <Carousel.Item>
+                <img
+                  className="d-block"
+                  src="http://i66.tinypic.com/141jsjd.jpg"
+                  alt="First slide"
+                />
+                <Carousel.Caption>
+                  <h3>First slide label</h3>
+                  <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block"
+                  src="http://i68.tinypic.com/kcjcw0.jpg"
+                  alt="Third slide"
+                />
+
+                <Carousel.Caption>
+                  <h3>Second slide label</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block"
+                  src="http://i68.tinypic.com/2vsnoec.jpg"
+                  alt="Third slide"
+                />
+
+                <Carousel.Caption>
+                  <h3>Third slide label</h3>
+                  <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+            </Carousel>
+          </div>
+          </div>
         </div>
       </div>
     );
