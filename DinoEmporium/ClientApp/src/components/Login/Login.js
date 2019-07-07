3ae75a935073f04a958fa70ab0094e7679746b0f
Michelle Beshears <meeshysu@gmail.com>
2019-07-06 19:48:31 -0500

    working on the styling of the login page

--- DinoEmporium/ClientApp/src/components/Login/Login.js
+++ DinoEmporium/ClientApp/src/components/Login/Login.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import { MDBContainer, MDBRow, MDBCol, MDBCard, MDBCardBody, MDBInput, MDBBtn, MDBIcon, MDBModalFooter } from 'mdbreact';
 import { Link } from 'react-router-dom';
-
 import authRequests from '../../firebaseRequests/auth';
+import LoginBanner from '../../Images//Login.png';
 
 import './Login.scss';
 
@@ -46,61 +47,100 @@ class Login extends React.Component {
 
   render () {
     const { user } = this.state;
-    return (
-      <div className="container Login">
-        <div className="loginForm" id="login-form">
-          <h1 className="text-center">Login</h1>
-          <form className="form-horizontal col-sm-6 col-sm-offset-3">
-            <div className="form-group">
-              <label htmlFor="inputEmail" className="col-sm-4 control-label">
-                Email:
-              </label>
-              <div className="col-sm-8">
-                <input
+   return (
+      <MDBContainer>
+        <MDBRow>
+          <MDBCol md="6">
+            <MDBCard>
+              <MDBCardBody className="mx-4">
+                <div className="text-center">
+                  <h3 className="dark-grey-text mb-5">
+                  <div><img className="login-image" src={LoginBanner} alt='login-banner'></img></div> 
+                    <strong>Sign in</strong>
+                  </h3>
+                </div>
+                <MDBInput
+                  label="Your email"
+                  group
                   type="email"
                   className="form-control"
                   id="inputEmail"
-                  placeholder="Email"
                   value={user.email}
                   onChange={this.emailChange}
+                  validate
+                  error="wrong"
+                  success="right"
                 />
-              </div>
-            </div>
-            <div className="form-group">
-              <label htmlFor="inputPassword" className="col-sm-4 control-label">
-                Password:
-              </label>
-              <div className="col-sm-8">
-                <input
+                <MDBInput
+                  label="Your password"
+                  group
                   type="password"
                   className="form-control"
                   id="inputPassword"
-                  placeholder="Password"
                   value={user.password}
                   onChange={this.passwordChange}
+                  validate
+                  containerClass="mb-0"
                 />
-              </div>
-            </div>
-            <div className="form-group">
-              <div className="col-sm-12 text-center">
-                <Link to="/register">Need to Register?</Link>
-              </div>
-            </div>
-            <div className="form-group">
-              <div className="col-sm-12">
-                <button
-                  type="submit"
-                  className="btn btn-default col-xs-12"
-                  onClick={this.loginClickEvent}
-                >
-                  Login
-                </button>
-              </div>
-            </div>
-          </form>
-          
-        </div>
-      </div>
+                <p className="font-small blue-text d-flex justify-content-end pb-3">
+                  Forgot
+                  <a href="#!" className="blue-text ml-1">
+  
+                    Password?
+                  </a>
+                </p>
+                <div className="text-center mb-3">
+                  <MDBBtn
+                    type="button"
+                    gradient="blue"
+                    rounded
+                    className="btn-block z-depth-1a"
+                    onClick={this.loginClickEvent}
+                  >
+                    Sign in
+                  </MDBBtn>
+                </div>
+                <p className="font-small dark-grey-text text-right d-flex justify-content-center mb-3 pt-2">
+  
+                  or Sign in with:
+                </p>
+                <div className="row my-3 d-flex justify-content-center">
+                  <MDBBtn
+                    type="button"
+                    color="white"
+                    rounded
+                    className="mr-md-3 z-depth-1a"
+                  >
+                    <MDBIcon fab icon="facebook-f" className="blue-text text-center" />
+                  </MDBBtn>
+                  <MDBBtn
+                    type="button"
+                    color="white"
+                    rounded
+                    className="mr-md-3 z-depth-1a"
+                  >
+                    <MDBIcon fab icon="twitter" className="blue-text" />
+                  </MDBBtn>
+                  <MDBBtn
+                    type="button"
+                    color="white"
+                    rounded
+                    className="z-depth-1a"
+                  >
+                    <MDBIcon fab icon="google-plus-g" className="blue-text" />
+                  </MDBBtn>
+                </div>
+              </MDBCardBody>
+              <MDBModalFooter className="mx-5 pt-3 mb-1">
+                <p className="font-small grey-text d-flex justify-content-end">
+                  Not a member?
+                  <Link className="blue-text ml-1" to="/register">Need to Register?</Link>
+                </p>
+              </MDBModalFooter>
+            </MDBCard>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
     );
   }
 }
