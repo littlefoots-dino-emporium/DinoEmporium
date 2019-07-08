8592b15d0c3a4fe3e133d0922d4bef3442cf753f
Lance Hamilton <lance.hamilton24@gmail.com>
2019-07-08 16:18:59 -0500

    wip

--- DinoEmporium/ClientApp/src/components/WishListItem/WishListItem.js
+++ DinoEmporium/ClientApp/src/components/WishListItem/WishListItem.js
@@ -1,12 +1,49 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import customerProductReq from '../../helpers/data/customerProductRequest';
+import wishList from '../../helpers/data/wishListRequest';
+import customerRequest from '../../helpers/data/customerRequest';
+import productTypeRequests from '../../helpers/data/productTypeRequest';
+import autheRequests from '../../firebaseRequests/auth';
 import './WishListItem.scss';
 
 export class WishListItem extends Component {
+
+    state = {
+        customer: '',
+        products: [],
+    }
+
     deleteKidEvent = (e) => {
         e.preventDefault();
         const { deleteOneProduct, customerProduct } = this.props;
         deleteOneProduct(customerProduct.productId);
       }
+
+      componentDidMount() {
+        let uid = autheRequests.getUid();
+        customerRequest.getCustomerProfile(uid).then((customer) => {
+            this.setState({ customer });
+          })
+        
+          productTypeRequests.getDinoRequest().then((products) => {
+            this.setState({ products });
+            this.setState({ filteredDinosaurs: products });
+          })
+
+    }
+
+      addToCart = () => {
+        const { customer } = this.state;
+        const {customerProduct} = this.props;
+
+        this.setState({ buttonTextChange: "In Cart", inCart: true });
+        const CustomerProductInfo = {
+            productId: customerProduct.id,
+            customerId: customer.id
+        }
+        customerProductReq.postCustomerProductRequest(CustomerProductInfo);
+        wishList.deleteSingleProduct(customerProduct.id)
+    }
       
   render() {
     const { customerProduct } = this.props;
@@ -24,7 +61,7 @@ export class WishListItem extends Component {
                 <h5>Size:{customerProduct.size}</h5>
                 <h5>Price:${customerProduct.price}</h5> 
                 <button className="btn btn-danger" onClick={this.deleteKidEvent}><i class="fas fa-trash-restore"></i></button>
-                <button className="btn btn-primary" onClick={this.deleteKidEvent}><i class="fas fa-cart"></i></button>
+                <button className="btn btn-primary" onClick={this.addToCart}><i class="fas fa-cart"></i></button>
             </div>
             
             </div>
