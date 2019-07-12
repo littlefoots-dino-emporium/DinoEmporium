fc3e1cba4fb7179c737f3c694fd9dcb19b1dcf52
Michelle Beshears <meeshysu@gmail.com>
2019-07-11 19:06:29 -0500

    sweaters and fences adjusted

--- DinoEmporium/ClientApp/src/components/FenceItem/FenceItem.js
+++ DinoEmporium/ClientApp/src/components/FenceItem/FenceItem.js
@@ -14,7 +14,7 @@ class FenceItem extends React.Component {
         customer: '',
         buttonTextChange: "Add To Cart",
         wishListButtonChange: "Add To Wish List",
-        inWishList: false, 
+        inWishList: false,
         inCart: false,
     }
     componentDidMount() {
@@ -23,14 +23,14 @@ class FenceItem extends React.Component {
         customerRequest.getCustomerProfile(uid).then((customer) => {
             console.log(customer.id, product.id);
             this.setState({ customer });
-          })
+        })
     }
     toggleModal = () => {
         this.setState({
             showModal: !this.state.showModal
         });
     }
-    
+
     addToCart = (productId) => {
         const { customer, inWishList } = this.state;
         const { product } = this.props;
@@ -50,46 +50,50 @@ class FenceItem extends React.Component {
         if (inCart === true) {
             alert("This item is already in your cart")
         } else {
-        this.setState({ wishListButtonChange: "In Wish List", inWishList: true });
-        const CustomerProductInfo = {
-            productId: product.id,
-            customerId: customer.id
+            this.setState({ wishListButtonChange: "In Wish List", inWishList: true });
+            const CustomerProductInfo = {
+                productId: product.id,
+                customerId: customer.id
+            }
+            wishList.postWishListRequest(CustomerProductInfo);
         }
-        wishList.postWishListRequest(CustomerProductInfo);
-      }   
     }
 
     render() {
         const { product } = this.props;
         return (
-        <div className="anotherDivForFences">
-            <div className="fenceModal">
-                <Button className="modal_opener" onClick={this.toggleModal}>
-                    <img className='fence-image' src={product.image} alt='product' />
-                    <p className='fence-title'><i>{product.title}</i></p>
-                    
-                </Button>
-
-                <Modal
-                    show={this.state.showModal}
-                    closeCallback={this.toggleModal}
-                    customClass="custom_modal_class"
-                >
-                    <React.Fragment>
-                        <div className='productCard'>
-                            <li className='fence-price'><i>${product.price} per foot.</i></li>
-                            <li className='fence-description'>{product.description}</li>
-                            <Button className="addToCart" onClick = {this.addToCart}>
+            <div className="anotherDivForFences">
+                <div className="fenceModal">
+                    <div className='productCard'>
+                        <img className='fence-image' src={product.image} alt='product' />
+                        <p className="all">
+                            <h2 className='fence-title'><i>{product.title}</i></h2>
+                            <p className='fence-price'><i>${product.price}</i></p>
+                            <p className='fence-quantity'>We have <b>{product.quantity}</b> in stock.</p>
+                            <Button className="addToCart btn-sm" onClick={this.addToCart}><i class="fas fa-luggage-cart"></i>
                                 {this.state.buttonTextChange}
                             </Button>
-                            <Button className="addToWishlist" onClick = {this.addToWishlist}>
+                            <Button className="addToWishList btn-sm" onClick={this.addToWishlist}><i class="fas fa-magic mr-1"></i>
                                 {this.state.wishListButtonChange}
                             </Button>
-                        </div>
-                    </React.Fragment>
-                </Modal>
+                            <Button className="modal_opener btn-sm" onClick={this.toggleModal}>
+                                <i class="far fa-question-circle"></i>
+                            </Button>
+                            <Modal
+                                show={this.state.showModal}
+                                closeCallback={this.toggleModal}
+                                customClass="custom_modal_class w-500"
+                            >
+                                <React.Fragment>
+
+                                    <li className='fence-description'>{product.description}</li>
+
+                                </React.Fragment>
+                            </Modal>
+                        </p>
+                    </div>
+                </div>
             </div>
-        </div>
         )
     }
 }
